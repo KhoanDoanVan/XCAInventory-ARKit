cc5defa00ca161d35f5858d06288a8b55e833484
KhoanDoanVan <doanvankhoan124@gmail.com>
2024-10-03 10:53:29 +0700

    Initial Commit

---
+++ XCAInventoryTracker/XCAInventoryTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCAInventoryTrackerApp.swift
+//  XCAInventoryTracker
+//
+//  Created by Đoàn Văn Khoan on 3/10/24.
+//
+
+import SwiftUI
+
+@main
+struct XCAInventoryTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
